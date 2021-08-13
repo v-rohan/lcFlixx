574fd3b9d4d935896308251d4deb472a2e4b4fd5
Sakshi <sakshi.051717@gmail.com>
2021-08-13 06:59:13 +0530

    comp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import logo from "../src/media/logoRed.png";
 import "./App.css";
 import video from "../src/media/videoo.mp4";
